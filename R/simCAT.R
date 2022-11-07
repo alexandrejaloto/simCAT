6521e0867bf3a2364e0ccdce95ee8efa8f109b42
Alexandre Jaloto <alexandrejaloto@gmail.com>
2022-11-07 17:42:14 -0300

    simCAT without replications

--- R/simCAT.R
+++ R/simCAT.R
@@ -3,9 +3,9 @@
 #'
 #' @description A CAT simulation with dicotomic items.
 #'
-#' @param resps list with response matrixes (0 and 1). Each
-#' matrix has the number of columns
-#'  corresponding to the number of items
+#' @param resps a matrix with responses (0 and 1).
+#' The number of columns
+#'  corresponds to the number of items
 #' @param bank matrix with item parameters (a, b, c)
 #' @param start.theta first theta
 #' @param sel.method item selection method
@@ -32,7 +32,6 @@
 #' \item `min.items` maximum number of items
 #' \item `fixed` fixed number of items
 #' }
-#' @param replications number of replications of the simulation
 #'
 #' @details
 #'
@@ -47,8 +46,7 @@ simCAT <- function(resps, bank, start.theta = 0, sel.method = 'MFI',
                    threshold = .30, rmax = 1,
                    content.names = NULL, content.props = NULL,
                    content.items = NULL, met.content = 'MCCAT',
-                   stop = list(se = .3, hypo = .015, hyper = Inf),
-                   replication = 1)
+                   stop = list(se = .3, hypo = .015, hyper = Inf))
 {
 
   # preparation ----
@@ -63,148 +61,144 @@ simCAT <- function(resps, bank, start.theta = 0, sel.method = 'MFI',
 
   # objects -----------------------------------------------
 
-  for (i in 1:replication)
-  {
-    # theta and se
-    score <- data.frame(matrix(ncol = 2))
-    # did the CAT congerge?  (for whole application)
-    convergence <- list()
-    # theta history (for whole application)
-    theta.history <- list()
-    # se history (for whole application)
-    se.history <- list()
-    # previous responses
-    prev.resps <- list()
+  # theta and se
+  score <- data.frame(matrix(ncol = 2))
+  # did the CAT congerge?  (for whole application)
+  convergence <- list()
+  # theta history (for whole application)
+  theta.history <- list()
+  # se history (for whole application)
+  se.history <- list()
+  # previous responses
+  prev.resps <- list()
 
-    # progress bar ----
-    print(paste0('Replication ', i))
-    bar <- txtProgressBar(min = 0, max = nrow(resps[[1]]), char = "|", style = 3)
+  # progress bar ----
+  bar <- txtProgressBar(min = 0, max = nrow(resps), char = "|", style = 3)
+
+  # simulation ----
+
+  for (person in 1:nrow(resps))
+    # for (person in 1:10)
+  {
+
+    # person <- 1
+
+    ## verificar a exposição dos itens -----------------------------------------
+
+    # if it is the first person
+    if (person == 1)
+    {
+      number_items_available <- 1:nrow(bank)
+      items_available <- bank
+
+    } else {
+
+      exposure <- exposure.rate(prev.resps, rownames(bank))
+
+      # select available items
+      number_items_available <- which (exposure$Freq <= rmax)
+      items_available <- bank[number_items_available,]
+    }
 
     # simulation ----
 
-    for (person in 1:nrow(resps[[1]]))
-      # for (person in 1:10)
+    end <- list(stop = FALSE)
+    administered <- NULL
+    theta.cat <- theta.hist <- start.theta
+    SE <- se.hist <- 1
+
+    while(!end$stop)
     {
 
-      # person <- 1
+      # select item ----
 
-      ## verificar a exposição dos itens -----------------------------------------
+      item_select <- select.item(
+        bank = items_available,
+        theta = theta.cat,
+        administered = administered,
+        sel.method = sel.method,
+        cat.type = cat.type,
+        threshold = threshold,
+        SE = SE,
+        acceleration = acceleration,
+        max.items = max.items,
+        content.names = content.names,
+        content.props = content.props,
+        content.items = content.items,
+        met.content = met.content
+      )
 
-      # if it is the first person
-      if (person == 1)
-      {
-        number_items_available <- 1:nrow(bank)
-        items_available <- bank
+      # item_select <- which(rownames(items_available) == item_select$name)
 
-      } else {
+      # update administered items
+      administered <- c(administered, item_select$item)
 
-        exposure <- exposure.rate(prev.resps, rownames(bank))
+      # estimate theta
+      # pattern: select from resps only the available items, and from them, the administered ones (and the person)
+      theta <- eap(
+        pattern = resps[,number_items_available][person,administered],
+        bank = items_available[administered,]
+      )
 
-        # select available items
-        number_items_available <- which (exposure$Freq <= rmax)
-        items_available <- bank[number_items_available,]
-      }
+      # update theta
+      theta.cat <- theta$theta
 
-      # simulation ----
+      # delta theta
+      delta.theta <- abs(theta.cat - theta.hist[length(theta.hist)])
 
-      end <- list(stop = FALSE)
-      administered <- NULL
-      theta.cat <- theta.hist <- start.theta
-      SE <- se.hist <- 1
+      # update theta history
+      theta.hist <- c(theta.hist, theta.cat)
 
-      while(!end$stop)
-      {
+      # update SE
+      SE <- theta$SE
 
-        # select item ----
+      # delta SE
+      delta.se <- se.hist[length(se.hist)] - SE
 
-        item_select <- select.item(
-          bank = items_available,
-          theta = theta.cat,
-          administered = administered,
-          sel.method = sel.method,
-          cat.type = cat.type,
-          threshold = threshold,
-          SE = SE,
-          acceleration = acceleration,
-          max.items = max.items,
-          content.names = content.names,
-          content.props = content.props,
-          content.items = content.items,
-          met.content = met.content
+      # update SE history
+      se.hist <- c(se.hist, SE)
+
+      # compute information for theta.cat
+      info <- calc.info(bank = items_available, theta = theta.cat)
+      info[administered] <- 0
+      info <- max(info)
+
+      # stop the CAT? ----
+
+      end <- stop.cat(
+        rule = stop,
+        current = list(
+          se = SE,
+          delta.theta = delta.theta,
+          info = info,
+          applied = length(administered),
+          delta.se = delta.se
         )
-
-        # item_select <- which(rownames(items_available) == item_select$name)
-
-        # update administered items
-        administered <- c(administered, item_select$item)
-
-        # estimate theta
-        # pattern: select from resps only the available items, and from them, the administered ones (and the person)
-        theta <- eap(
-          pattern = resps[[i]][,number_items_available][person,administered],
-          bank = items_available[administered,]
-        )
-
-        # update theta
-        theta.cat <- theta$theta
-
-        # delta theta
-        delta.theta <- abs(theta.cat - theta.hist[length(theta.hist)])
-
-        # update theta history
-        theta.hist <- c(theta.hist, theta.cat)
-
-        # update SE
-        SE <- theta$SE
-
-        # delta SE
-        delta.se <- se.hist[length(se.hist)] - SE
-
-        # update SE history
-        se.hist <- c(se.hist, SE)
-
-        # compute information for theta.cat
-        info <- calc.info(bank = items_available, theta = theta.cat)
-        info[administered] <- 0
-        info <- max(info)
-
-        # stop the CAT? ----
-
-        end <- stop.cat(
-          rule = stop,
-          current = list(
-            se = SE,
-            delta.theta = delta.theta,
-            info = info,
-            applied = length(administered),
-            delta.se = delta.se
-          )
-        )
-
-      }
-
-      # store results
-      score[person,] <- c(theta.cat, SE)
-      convergence[[person]] <- end$convergence
-      theta.history[[person]] <- theta.hist
-      se.history[[person]] <- se.hist
-      prev.resps[[person]] <- rownames(items_available)[administered]
-
-      # progress bar
-      setTxtProgressBar(bar, person)
+      )
 
     }
 
-    names (score) <- c('theta', 'SE')
+    # store results
+    score[person,] <- c(theta.cat, SE)
+    convergence[[person]] <- end$convergence
+    theta.history[[person]] <- theta.hist
+    se.history[[person]] <- se.hist
+    prev.resps[[person]] <- rownames(items_available)[administered]
 
-    results[[i]] <- list(
-      score = score,
-      convergence = convergence,
-      theta.history = theta.history,
-      se.history = se.history,
-      prev.resps = prev.resps
-    )
+    # progress bar
+    setTxtProgressBar(bar, person)
+
   }
+
+  names (score) <- c('theta', 'SE')
+
+  results[[i]] <- list(
+    score = score,
+    convergence = convergence,
+    theta.history = theta.history,
+    se.history = se.history,
+    prev.resps = prev.resps
+  )
   return(results)
 }
 
